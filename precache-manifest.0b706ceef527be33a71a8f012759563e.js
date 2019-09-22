01ab8950639139b9a3887bb1050c46d91b75dd4a
Alibriaan <dimulka62@gmail.com>
2019-09-22 20:11:23 +0300

    Updates

---
+++ precache-manifest.0b706ceef527be33a71a8f012759563e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a88e31e38721826fe67fcde248d677c0",
+    "url": "/square-game/index.html"
+  },
+  {
+    "revision": "5caf40fc7f797742daf1",
+    "url": "/square-game/static/css/main.af240bc4.chunk.css"
+  },
+  {
+    "revision": "54e6bd55eb46697569da",
+    "url": "/square-game/static/js/2.a68a7a01.chunk.js"
+  },
+  {
+    "revision": "5caf40fc7f797742daf1",
+    "url": "/square-game/static/js/main.d21603e3.chunk.js"
+  },
+  {
+    "revision": "8b7c4c3fd4396de57de2",
+    "url": "/square-game/static/js/runtime-main.538e1d06.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/square-game/static/media/logo.25bf045c.svg"
+  }
+]);
